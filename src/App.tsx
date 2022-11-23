ead0f3f9fba77a431407203ef1bd556891f1b213
Leonardo Balsalobre <oliveirabalsa2@gmail.com>
2022-11-23 13:32:19 -0300

    feat: setInterval to refresh backend

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Metrics from './pages/Metrics';
-
 function App() {
   return (
     <BrowserRouter>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import SongGameCard from '../components/organisms/SongGameCard';
+import { http } from '../services/axios/http';
 
 function Home() {
+  const minuteInMiliseconds = 1000 * 60;
+  const fortyMinutes = minuteInMiliseconds * 40;
+  setInterval(async () => {
+    await http.get("/")
+}, fortyMinutes)
   return (
     <SongGameCard />
   );
